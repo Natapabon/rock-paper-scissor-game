cacf7d406eee734b7954f756e572a1f08b4dd140
Nata Pabon <ta@MacBook-Air-de-Ta.local>
2021-03-17 21:27:59 -0400

    Building out looping functionality

--- game.py
+++ game.py
@@ -13,59 +13,108 @@ choices = ["rock", "paper", "scissors"]
 # lifes check infor for step 4
 player_lives = 3
 computer_lives = 3
-total_lifes = 3
+total_lives = 3
 
-# Version 1, to explain array indexing
-# player_choice = choices[1]
-# print("index 1 in the choice array is " + player_choice + ", which is paper")
+# Step 5.5: looping life counter until 0
+#True or False are boolean data types
+# essentially, booleans are equivalent to an on or off switch, 1 or 0.
+player_choice = False
 
-player_choice = input("Choose rock, paper, or scissors: ")
+# define a win or lose function
+def winorlose(status):
+	# version 1 of function
+	# print("Inside winorlose function; sattus is: ", status)
+	
+	print("You ", status, ", Would you like to play again?")
+	choice = input("Y/N? ")
 
-print("user chose " + player_choice)
+	if choice == "N" or choice == "n":
+		print("You chose to quit. Better luck next time!")
+		exit()
 
-# step 2
-# this will be the AI Choise -> a random pick from the choices array
-computer_choice = choices[randint(0, 2)]
+	elif choice == "Y" or choice == "y":
+		# reset the player lives and computer lives
+		# and reset player choice to False, so our loop restarts
+		global player_lives
+		global computer_lives
+		global total_lives
 
-print("computer_choice: " + computer_choice)
+		player_lives = total_lives
+		computer_lives = total_lives
 
-# step 3
-
-if computer_choice == player_choice:
-	print("tie")
-
-elif computer_choice == "rock":
-	if player_choice == "scissors":
-		print("You lose!")
-		#step 4 
-		#verbose way
-		# player_lives = player_lives - 1
-		#simplified way
-		player_lives -= 1
 	else:
-		print("You win!")
-		computer_lives -= 1
+		print("Make a valid choice - Y or N")
+		# this might generate a bug tht we need to fix later
+		choice = input("Y/N? ")
+
+# player_choice == False
+while player_choice is False:
+	print("============/* RPS GAME */=============")
+	print("Computer Lives: ", computer_lives, "/", total_lives)
+	print("Player Lives: ", player_lives, "/", total_lives)
+	print("=======================================")
+
+	# Version 1, to explain array indexing
+	# player_choice = choices[1]
+	# print("index 1 in the choice array is " + player_choice + ", which is paper")
+
+	player_choice = input("Choose rock, paper, or scissors: ")
+	#player_choice now equals TRUE ->it has a value
+
+	print("user chose " + player_choice)
+
+	# step 2
+	# this will be the AI Choise -> a random pick from the choices array
+	computer_choice = choices[randint(0, 2)]
+
+	print("computer_choice: " + computer_choice)
+
+	# step 3
+
+	if computer_choice == player_choice:
+		print("tie")
+
+	elif computer_choice == "rock":
+		if player_choice == "scissors":
+			print("You lose!")
+			#step 4 
+			#verbose way
+			# player_lives = player_lives - 1
+			#simplified way
+			player_lives -= 1
+		else:
+			print("You win!")
+			computer_lives -= 1
 
 
-elif computer_choice == "paper":
-	if player_choice == "rock":
-		print("You lose!")
-		player_lives -= 1
-	else:
-		print("You win!")
-		computer_lives -= 1
+	elif computer_choice == "paper":
+		if player_choice == "rock":
+			print("You lose!")
+			player_lives -= 1
+		else:
+			print("You win!")
+			computer_lives -= 1
 
 
-elif computer_choice == "scissors":
-	if player_choice == "paper":
-		print("You lose!")
-		player_lives -= 1
-	else:
-		print("You win!")
-		computer_lives -= 1
+	elif computer_choice == "scissors":
+		if player_choice == "paper":
+			print("You lose!")
+			player_lives -= 1
+		else:
+			print("You win!")
+			computer_lives -= 1
 
-# Step 5: life counter message
-print("Player lives: ", player_lives)
-print("Computer lives: ", computer_lives)
+	# Step 6: life counter goes to 0
+	if player_lives == 0:
+		winorlose("lose")
+		
+	if computer_lives == 0:
+		winorlose("won")
+		
+	# Step 5: life counter message
+	print("Player lives: ", player_lives)
+	print("Computer lives: ", computer_lives)
 
-
+	# map the loop keep running, by setting player_choice back to False
+	# unset, so that ourloop condition will evaluate to True
+	player_choice = False
